98d0498f5c4a439537a1d1c7757d87587ffe3b3e
Zulema Jimenez <zcjime02@gmail.com>
2019-05-17 07:58:30 -0500

    Borrando el contenido del MainActivity

--- app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     private FABToolbarLayout morph;
 
@@ -16,31 +16,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_label);
-        morph = (FABToolbarLayout) findViewById(R.id.menu_fab);
-
-        View uno, dos, tres, cuatro;
-
-        uno = findViewById(R.id.accion_favorito);
-        dos = findViewById(R.id.accion_buscar);
-        cuatro = findViewById(R.id.accion_carrito);
-
-        fab.setOnClickListener(this);
-        uno.setOnClickListener(this);
-        dos.setOnClickListener(this);
-        cuatro.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.menu_fab) {
-            morph.show();
-        }
-
-        morph.hide();
     }
 }
